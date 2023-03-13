8cbe2324bcebf79fb31c3e336516a3af6ddac0f7
葉奕緯 <aaa24295234@gmail.com>
2023-03-14 00:54:06 +0800

    swiper and dynamic img

--- pages/vol-star.vue
+++ pages/vol-star.vue
@@ -21,22 +21,23 @@
     </div>
     <div class="vol-rotate">
       <div class="vol-rotate-outer">
-        
-          <!-- <div class="swiper-container" ref="swiperRef" v-for="(article,index) in articles" :key="index">
-            <router-link to="/stars/01" class="vol-rotate-box">
-              <div class="vol-rotate-img"></div>
-              <div class="vol-rotate-head">{{ article.head }}</div>
-              <div class="vol-rotate-text">{{ article.text }}</div>
-            </router-link>
-          </div> -->
 
          <div class="swiper">
             <!-- Additional required wrapper -->
             <div class="swiper-wrapper">
               <!-- Slides -->
-              <div class="swiper-slide">Slide 1</div>
-              <div class="swiper-slide">Slide 2</div>
-              <div class="swiper-slide">Slide 3</div>
+              <div v-for="(article, index) in articles"
+                :key="index"
+                class="swiper-slide"
+              >
+                <router-link to="/stars/01" class="vol-rotate-box">
+                  <div :style="{ backgroundImage: `url(${useAsset(article.img)})` }"
+                    class="vol-rotate-img"
+                  ></div>
+                  <div class="vol-rotate-head">{{ article.head }}</div>
+                  <div class="vol-rotate-text">{{ article.text }}</div>
+                </router-link>
+              </div>
             </div>
             <!-- If we need pagination -->
             <div class="swiper-pagination"></div>
@@ -48,12 +49,7 @@
             <!-- If we need scrollbar -->
             <div class="swiper-scrollbar"></div>
           </div>
-
-        <!-- <router-link to="/stars/01" class="vol-rotate-box">
-          <div class="vol-rotate-img"></div>
-          <div class="vol-rotate-head">每座燈塔都是獨一無二的眼睛</div>
-          <div class="vol-rotate-text">「聽說那兒有個燈塔，失戀的人都喜歡去，把不開心的東西留下。」 這是電影《春光乍泄》中，黎耀輝（梁朝偉）曾說過的話...</div>
-        </router-link> -->
+        
       </div>
       <div class="vol-rotate-bg">
       </div>
@@ -63,11 +59,12 @@
 
 <script setup>
 
+// https://stackblitz.com/edit/github-fjh4sx-m3hnpu?file=components%2Fdemo%2FDemoFirst.vue
+
 import Swiper, {
   Navigation,
   Pagination,
   Autoplay,
-  SwiperOptions,
 } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -82,7 +79,7 @@ onMounted(() => {
     loop: true,
     loopFillGroupWithBlank: true,
     autoHeight: false,
-    width: 600,
+    width: 330,
     autoplay: true,
     centeredSlides: true,
     enabled: true,
@@ -98,26 +95,41 @@ onMounted(() => {
   });
 });
 
+// https://github.com/nuxt/nuxt/issues/14766
+
 onBeforeUnmount(() => {
   engine?.destroy?.();
 });
 
+function useAsset(path) {
+  const assets = import.meta.glob('~/assets/**/*', {
+    eager: true,
+    import: 'default',
+  })
+  // @ts-expect-error: wrong type info
+  return assets['/assets/' + path]
+}
+
+
 
 const articles = reactive([
   {
-    head: '每座燈塔都是獨一無二的眼睛',
-    text: '',
-    href: '「聽說那兒有個燈塔，失戀的人都喜歡去，把不開心的東西留下。」'
+    head: '每座燈塔都是獨一無二的眼睛1',
+    text: '「聽說那兒有個燈塔，失戀的人都喜歡去，把不開心的東西留下。」',
+    href: '',
+    img: 'img/star/author1.png'
   },
   {
-    head: '每座燈塔都是獨一無二的眼睛',
-    text: '',
-    href: '「聽說那兒有個燈塔，失戀的人都喜歡去，把不開心的東西留下。」'
+    head: '每座燈塔都是獨一無二的眼睛2',
+    text: '「聽說那兒有個燈塔，失戀的人都喜歡去，把不開心的東西留下。」',
+    href: '',
+    img: 'img/star/author2.jpg'
   },
   {
-    head: '每座燈塔都是獨一無二的眼睛',
-    text: '',
-    href: '「聽說那兒有個燈塔，失戀的人都喜歡去，把不開心的東西留下。」'
+    head: '每座燈塔都是獨一無二的眼睛3',
+    text: '「聽說那兒有個燈塔，失戀的人都喜歡去，把不開心的東西留下。」',
+    href: '',
+    img: 'img/star/author3.jpg'
   },
 ])
 
@@ -193,7 +205,7 @@ const articles = reactive([
     &-img {
       width: 330px;
       height: 300px;
-      background-image: url('../assets/img/index/vol_one.png');
+      background-color: #999999;
       background-repeat: no-repeat;
       background-size: cover;
       background-position-x: center;
--- plugins/swiper.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-// import VueAwesomeSwiper from 'vue-awesome-swiper'
-// import 'swiper/swiper.css'
-
-export default defineNuxtPlugin(nuxtApp => {
-    // nuxtApp.vueApp.use(VueAwesomeSwiper)
-  })
